c5e74d4749204be9690ff8356425eb1de40eab42
Felipe <felipdetavares@gmail.com>
2024-05-19 19:21:06 -0300

    Ajuste Logo

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,9 +1,9 @@
 <template>
   <header class="bg-black text-white w-full p-4 pl-10 fixed z-10">
     <div class="container flex items-center gap-10">
-      <h1 class="text-2xl font-bold">E-Flix</h1>
+      <h1 class="text-2xl font-bold">MovieFlix</h1>
       <nav>
-        <ul class="flex space-x-4 items-center">
+        <ul class="flex space-x-4 items-center gap-4">
           <li><a href="/" class="hover:underline">Home</a></li>
           <li><a href="/filmes" class="hover:underline">Filmes</a></li>
           <li><a href="/series" class="hover:underline">Séries</a></li>
